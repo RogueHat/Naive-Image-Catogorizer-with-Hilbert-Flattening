447719134084a6286a265b226b3219437b99964f
Carlo <carlomiguelh@gmail.com>
2017-08-22 00:56:27 -0500

    init

---
+++ mymethods.py
@@ -0,0 +1,16 @@
+import numpy
+import hilbert_curve
+
+
+side = 32
+newshape = side*side
+
+hilbert_indexs = range(0,newshape)
+hilbert_map = map(lambda x: hilbert_curve.d2xy(newshape, x), hilbert_indexs)
+
+def arrange(img):
+	hilbert_flat = map(lambda x: numpy.array(img[hilbert_map[x]]), hilbert_indexs)
+	hilbert_flat = numpy.array(hilbert_flat)
+	return hilbert_flat
+	
+
